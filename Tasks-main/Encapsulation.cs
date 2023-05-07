4c9499b7750489fa30e707c4e725626bfb52f605
Samir <vliyevsamir385@gmail.com>
2023-05-07 12:44:51 +0400

    elave etdim

---
+++ Tasks-main/Encapsulation.cs
@@ -0,0 +1,18 @@
+# Tasks
+
+using System;
+using static System.Console
+using static System.Convert
+
+int a = 1;
+int b = 3;
+int c = a + b;
+WriteLine(c);
+
+//Samir Veliyevin yeni Reposu
+
+int d = 7;
+// deneme git add -A
+
+//Salam Dunya 26
+    
